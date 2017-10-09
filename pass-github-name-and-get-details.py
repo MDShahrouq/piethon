bc8ea8bdf511f93f3dbd95af3ab51518a4d13b03
MD Shahrouq <mdshah1994@gmail.com>
2017-10-09 17:43:40 +0530

    Update, Good presentation

--- pass-github-name-and-get-details.py
+++ pass-github-name-and-get-details.py
@@ -6,18 +6,19 @@ import json
 import sys
 
 
-print "To get My Github INfo using Github API"
-
 url = "https://api.github.com/users/"+str(sys.argv[1])
 
-print "My Github Profile API : "+url
+print "Fetching Details of:"+str(sys.argv[1])
+print "\nGithub API Url: "+url
 
 request = urllib.urlopen(url).read()
+
 bio = json.loads(request)
 
 #IpAddress = re.findall(r"\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b",request)
 
 #print "My Github profile info is: ",bio
-
-print "",bio["name"],bio["bio"],bio["location"]
-
+print "\nName:",bio["name"]
+print "\nBio:",bio["bio"]
+print "\nLocation:",bio["location"]	
+print "\nProfile url:",bio["url"]+"\n"
