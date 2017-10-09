29bf99e5ae2486161045f39ea7690fa302a7eccc
MD Shahrouq <mdshah1994@gmail.com>
2017-10-09 18:16:03 +0530

    Error if no user present

--- pass-github-name-and-get-details.py
+++ pass-github-name-and-get-details.py
@@ -4,20 +4,23 @@ import urllib
 import re
 import json
 import sys
+import requests 
+#Run this Code , passing Github username Eg, python pass-github-name-and-get-details.py <username>
+#to check the Username exists or no
+try:
+  url = requests.get('https://api.github.com/users/'+str(sys.argv[1]))
+  url.raise_for_status()
+except requests.exceptions.HTTPError as err:
+  print err
+  sys.exit(1)
+
+print "\nFetching Details of:"+str(sys.argv[1])
+print "\nGithub API Url: "+"https://api.github.com/users/"+str(sys.argv[1])
+
+#request = urllib.urlopen(url).read()
+bio = json.loads(url.content)
 
 
-url = "https://api.github.com/users/"+str(sys.argv[1])
-
-print "Fetching Details of:"+str(sys.argv[1])
-print "\nGithub API Url: "+url
-
-request = urllib.urlopen(url).read()
-
-bio = json.loads(request)
-
-#IpAddress = re.findall(r"\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b",request)
-
-#print "My Github profile info is: ",bio
 print "\nName:",bio["name"]
 print "\nBio:",bio["bio"]
 print "\nLocation:",bio["location"]	
